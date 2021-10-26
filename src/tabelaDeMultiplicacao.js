9cecaa1da0fee148d40619502b78a221f9fb6b06
Thiago <thig90.dev@gmail.com>
2021-10-25 22:30:16 -0300

    Atividade 45 - Tabela de Multiplicação

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,13 @@
-function nomeDaFuncao() { 
+function tabelaMultiplicacao(n) { 
     // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
-    }
-    console.table(x)
-}
+    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e mostrar o restante da tabela no console.
+    let multArr = [];
+    for (let linha = 0; linha <= n; linha++) {
+        multArr.push(linha);
+        console.log(multArr)
+        for (let coluna = 0; coluna <= n; coluna++) {
+            multArr.push([coluna]);
+            }
+        }
+        console.table(multArr);
+} console.log(tabelaMultiplicacao(10));
